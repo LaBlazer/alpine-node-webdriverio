a2a2ba06dca2b83a2233f33010021d70a3ce3f78
Matus <matus.ozaniak@profiq.com>
2019-06-18 10:09:53 +0200

    change

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 jv=$(java -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)
 if [ $jv -ge 11 ]; then
+    echo ok
     exit 0
 else
+    echo not ok
     exit 1
 fi
