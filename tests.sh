8b556569e0bc2160800347e07b3470a1a0307a9f
Jan Turon <turon.honza@gmail.com>
2019-08-16 17:59:21 +0200

    Fix alpine installation

--- tests.sh
+++ tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 jv=$(java -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)
-ch=$(google-chrome --version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1 | sed '/Google Chrome/s///')
+ch=$(chromium --version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1 | sed '/Google Chrome/s///')
 
 if [ $jv -ge 8 ] && [ $ch -ge 74 ]; then
     echo ok
